db6a21e27132fafdb7e77de15da69d731bed4eb1
Nicholas Ramsay <nick@yousponsor.me>
2021-05-05 20:16:31 +1000

    added add subject menu

--- src/main.c
+++ src/main.c
@@ -6,6 +6,23 @@
 #include "include/Subject.h"
 #include "include/SubjectsFile.h"
 
+void getLine(char* str) {
+
+    char c;
+    int i = 0;
+
+    while(1) { 
+        c = getc(stdin);
+
+        if (c == '\n') {
+            str[i] = '\0';
+            return;
+        } else {
+            str[i++] = c;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 
     // read subjects into array of subjects
@@ -48,9 +65,88 @@ int main(int argc, char** argv) {
                 if (strcmp(argv[2], "add") == 0) {
                     if (argc > 3) {
                         if (!doesSubjectExistByName(subjectsFile, argv[3])) {
-                            // add subject
-                            // get name
-                            // get -a for assessment -a <name> <weight> <value|empty>
+
+                            // begin create subject menu
+                            printf("Creating a subject of name \"%s\".\n Now adding assignments to \"default\" estimation.\nPlease enter each assignment and its information separated by a newline, assignment information is of the format:\n\n\t<assignment name> <weight> <value>\n\nOnce you are done, enter an empty line to continue.\n", argv[3]);
+
+                            // declare some vars to store obtained assinment information
+                            char asn_inf[50], asn_name[30], asn_weight_str[10], asn_value_str[10];
+                            float asn_weight, asn_value;
+
+                            Assessment assessments[20];
+                            int current_assessment = 0;
+
+                            // store the part of the assignment we are reading i.e. name, weight or value
+                            int entry_part = 0;
+
+                            // loop until all assignment details provided
+                            while(1) {
+
+                                // TODO: clear vals
+                                // TODO: fix bug: this only clears the first letter
+                                // reset all strings
+                                strcpy(asn_inf, "");
+                                strcpy(asn_name, "");
+                                strcpy(asn_weight_str, "");
+                                strcpy(asn_value_str, "");
+
+                                // get the user info
+                                getLine(&asn_inf);
+                                printf("%d\n", strlen(asn_inf));
+
+                                // first check if they are finished (minimum entry is 5 chars long)
+                                if (strlen(asn_inf) < 5) {
+                                    printf("breaking\n");
+                                    break;
+                                }
+
+                                // reset the entry part to the assignment name
+                                entry_part = 0;
+
+                                // loop through the chars in the line
+                                for(int i = 0; i < strlen(asn_inf); i++) {
+
+                                    // ensure we don't write separators into the strings
+                                    // check if we reached end of entry part and increment to move to the next part
+                                    // TODO: check for multiple spaces
+                                    if (asn_inf[i] == ' ') {
+                                        i++;
+                                        entry_part++;
+                                    }
+
+                                    // check what part we are reading
+                                    if (entry_part == 0) {
+                                        // we are still obtaining the assignment name
+                                        printf("num: %d\n", strlen(asn_name));
+                                        asn_name[strlen(asn_name)] = asn_inf[i];
+                                    } else if (entry_part == 1) {
+                                        // we are obtaining the assignment name
+                                        asn_weight_str[strlen(asn_weight_str)] = asn_inf[i];
+                                    } else if (entry_part == 2) {
+                                        // we are obtaining the assignment name
+                                        asn_value_str[strlen(asn_value_str)] = asn_inf[i];
+                                    } else {
+                                        // we finished the line break the for loop
+                                        break;
+                                    }
+                                }
+
+                                // now convert asn_weight_str and asn_value_str to floats
+                                asn_weight = (float) atof(asn_weight_str);
+                                asn_value = (float) atof(asn_value_str);
+                                printf("(%s-%s-%s)\n", asn_name, asn_weight_str, asn_value_str);
+
+                                // now add the assignment details to assessment array
+                                assessments[current_assessment++] = *createAssessment(asn_name, asn_weight, asn_value);
+                                //assessments[current_assessment - 1]
+                                
+                            }
+
+                            for (int i = 0; i < current_assessment; i++) {
+                                printf("  * %s: %f, %f\n", assessments[i].name, assessments[i].weight, assessments[i].value);
+                            }
+                            
+
                         } else {
                             printf("Error: A subject already exists with name \"%s\".\n", argv[3]);
                         }
