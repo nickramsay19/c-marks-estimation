cb2253156547f8481d3201c704676f611f04dde4
Nicholas Ramsay <nick@yousponsor.me>
2021-06-16 12:46:29 +1000

    moved arg parsing defs to header, currently compiling as expected

--- src/include/ArgumentParser.h
+++ src/include/ArgumentParser.h
@@ -1,77 +1,77 @@
-// #ifndef ARGUMENT_PARSER_H
-// #define ARGUMENT_PARSER_H
+#ifndef ARGUMENT_PARSER_H
+#define ARGUMENT_PARSER_H
 
-// #include <stdlib.h>
-// #include "argp/argp.h"
+#include <stdlib.h>
+#include "argp/argp.h"
 
-// const char *argp_program_version = "argp-ex3 1.0";
-// const char *argp_program_bug_address = "<bug-gnu-utils@gnu.org>";
+const char *argp_program_version = "argp-ex3 1.0";
+const char *argp_program_bug_address = "<bug-gnu-utils@gnu.org>";
 
-// // Program documentation.
-// static char doc[] = "Argp example #3 -- a program with options and arguments using argp";
+// Program documentation.
+static char doc[] = "Argp example #3 -- a program with options and arguments using argp";
 
-// // A description of the arguments we accept.
-// static char args_doc[] = "ARG1 ARG2";
+// A description of the arguments we accept.
+static char args_doc[] = "ARG1 ARG2";
 
-// // The options we understand.
-// static struct argp_option options[] = {
-//   {"verbose",  'v', 0,      0,  "Produce verbose output" },
-//   {"quiet",    'q', 0,      0,  "Don't produce any output" },
-//   {"silent",   's', 0,      OPTION_ALIAS },
-//   {"output",   'o', "FILE", 0,
-//    "Output to FILE instead of standard output" },
-//   { 0 }
-// };
+// The options we understand.
+static struct argp_option options[] = {
+  {"verbose",  'v', 0,      0,  "Produce verbose output" },
+  {"quiet",    'q', 0,      0,  "Don't produce any output" },
+  {"silent",   's', 0,      OPTION_ALIAS },
+  {"output",   'o', "FILE", 0,
+   "Output to FILE instead of standard output" },
+  { 0 }
+};
 
-// // Used by main to communicate with parse_opt.
-// struct arguments
-// {
-//   char *args[2]; // arg1 & arg2 
-//   int silent, verbose;
-//   char *output_file;
-// };
+// Used by main to communicate with parse_opt.
+struct arguments
+{
+  char *args[2]; // arg1 & arg2 
+  int silent, verbose;
+  char *output_file;
+};
 
-// // Parse a single option.
-// static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+// Parse a single option.
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
   
-//     // Get the input argument from argp_parse, which we
-//     // know is a pointer to our arguments structure.
-//     struct arguments *arguments = state->input;
+    // Get the input argument from argp_parse, which we
+    // know is a pointer to our arguments structure.
+    struct arguments *arguments = state->input;
 
-//     switch (key) {
-//         case 'q': case 's':
-//             arguments->silent = 1;
-//             break;
-//         case 'v':
-//             arguments->verbose = 1;
-//             break;
-//         case 'o':
-//             arguments->output_file = arg;
-//             break;
+    switch (key) {
+        case 'q': case 's':
+            arguments->silent = 1;
+            break;
+        case 'v':
+            arguments->verbose = 1;
+            break;
+        case 'o':
+            arguments->output_file = arg;
+            break;
 
-//         case ARGP_KEY_ARG:
-//             if (state->arg_num >= 2) {
-//                 // Too many arguments.
-//                 argp_usage (state);
-//                 arguments->args[state->arg_num] = arg;
-//             }
-//             break;
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= 2) {
+                // Too many arguments.
+                argp_usage (state);
+                arguments->args[state->arg_num] = arg;
+            }
+            break;
 
-//         case ARGP_KEY_END:
-//             if (state->arg_num < 2) {
-//                 // Not enough arguments.
-//                 argp_usage (state);
-//             }
-//             break;
+        case ARGP_KEY_END:
+            if (state->arg_num < 2) {
+                // Not enough arguments.
+                argp_usage (state);
+            }
+            break;
 
-//         default:
-//             return ARGP_ERR_UNKNOWN;
-//     }
-//     return 0;
-// }
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
 
 
-// // Our argp parser
-// static struct argp Argp = { options, parse_opt, args_doc, doc };
+// Our argp parser
+static struct argp Argp = { options, parse_opt, args_doc, doc };
 
-// #endif
+#endif
