e3831a5ea3aae8f140e54aba6109a9480ae23a68
Nicholas Ramsay <nick@yousponsor.me>
2021-06-16 13:49:48 +1000

    compilation now works as desired, arg parser vars now in c files

--- src/ArgumentParser.c
+++ src/ArgumentParser.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 #include "include/argp/argp.h"
 
+const char *argp_program_version = "argp-ex3 1.0";
+const char *argp_program_bug_address = "<bug-gnu-utils@gnu.org>";
+
+// Program documentation.
+char doc[] = "Argp example #3 -- a program with options and arguments using argp";
+
+// A description of the arguments we accept.
+char args_doc[] = "ARG1 ARG2";
+
+// The options we understand.
+struct argp_option options[] = {
+    {"verbose",  'v', 0,      0,  "Produce verbose output" },
+    {"quiet",    'q', 0,      0,  "Don't produce any output" },
+    {"silent",   's', 0,      OPTION_ALIAS },
+    {"output",   'o', "FILE", 0,
+    "Output to FILE instead of standard output" },
+    { 0 }
+};
+
 // Parse a single option.
 error_t parse_opt(int key, char *arg, struct argp_state *state) {
   
@@ -41,3 +60,6 @@ error_t parse_opt(int key, char *arg, struct argp_state *state) {
     }
     return 0;
 }
+
+// Our argp parser
+struct argp Argp = { options, parse_opt, args_doc, doc };
--- src/include/ArgumentParser.h
+++ src/include/ArgumentParser.h
@@ -4,37 +4,15 @@
 #include <stdlib.h>
 #include "argp/argp.h"
 
-//const char *argp_program_version = "argp-ex3 1.0";
-//const char *argp_program_bug_address = "<bug-gnu-utils@gnu.org>";
-
-// Program documentation.
-char doc[] = "Argp example #3 -- a program with options and arguments using argp";
-
-// A description of the arguments we accept.
-char args_doc[] = "ARG1 ARG2";
-
-// The options we understand.
-struct argp_option options[] = {
-  {"verbose",  'v', 0,      0,  "Produce verbose output" },
-  {"quiet",    'q', 0,      0,  "Don't produce any output" },
-  {"silent",   's', 0,      OPTION_ALIAS },
-  {"output",   'o', "FILE", 0,
-   "Output to FILE instead of standard output" },
-  { 0 }
-};
-
 // Used by main to communicate with parse_opt.
 struct arguments
 {
-  char *args[2]; // arg1 & arg2 
-  int silent, verbose;
-  char *output_file;
+    char *args[2]; // arg1 & arg2 
+    int silent, verbose;
+    char *output_file;
 };
 
 // Parse a single option.
 error_t parse_opt(int key, char *arg, struct argp_state *state);
-
-// Our argp parser
-struct argp Argp = { options, parse_opt, args_doc, doc };
 
 #endif
