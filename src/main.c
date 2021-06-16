a3cf18bddcec56b5bc658c62335dfc8ac44b4b67
Nicholas Ramsay <nick@yousponsor.me>
2021-06-16 14:02:01 +1000

    arg parser is no header only, this is the best implementation working, compilation works

--- src/main.c
+++ src/main.c
@@ -11,30 +11,30 @@
 
 int main(int argc, char** argv) {
 
-    // //read subjects into array of subjects
-    // SubjectsFile* subjectsFile = createSubjectsFile("/Users/nickramsay/Documents/Projects/c-marks-estimation/dist/subjects.bin");
+    //read subjects into array of subjects
+    SubjectsFile* subjectsFile = createSubjectsFile("/Users/nickramsay/Documents/Projects/c-marks-estimation/dist/subjects.bin");
 
-    // //use argp
-    // struct arguments arguments;
+    // use argp
+    struct arguments arguments;
 
-    // //Default values.
-    // arguments.silent = 0;
-    // arguments.verbose = 0;
-    // arguments.output_file = "-";
+    //Default values.
+    arguments.silent = 0;
+    arguments.verbose = 0;
+    arguments.output_file = "-";
 
-    // //Parse our arguments; every option seen by parse_opt will
-    // //be reflected in arguments.
-    // argp_parse(&Argp, argc, argv, 0, 0, &arguments);
+    //Parse our arguments; every option seen by parse_opt will
+    //be reflected in arguments.
+    argp_parse(&Argp, argc, argv, 0, 0, &arguments);
 
-    // printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
-    //         "VERBOSE = %s\nSILENT = %s\n",
-    //         arguments.args[0], arguments.args[1],
-    //         arguments.output_file,
-    //         arguments.verbose ? "yes" : "no",
-    //         arguments.silent ? "yes" : "no");
+    printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+            "VERBOSE = %s\nSILENT = %s\n",
+            arguments.args[0], arguments.args[1],
+            arguments.output_file,
+            arguments.verbose ? "yes" : "no",
+            arguments.silent ? "yes" : "no");
 
-    // //free the subjects file
-    // freeSubjectsFile(subjectsFile);
+    //free the subjects file
+    freeSubjectsFile(subjectsFile);
 
     return 0;
 }
